ab66d5d3dcdcf2f107873662047b766e7ff5a647
Douglas Usuriaga <douglasusupa2015@gmail.com>
2024-10-27 17:44:10 -0500

    pages problematics created

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,15 @@ import Welcome from "./Pages/Welcome/Welcome";
 import Page404 from "./pages/page404/page404";
 import Home from "./pages/home/Home";
 import "./index.css"
+import Problematicas from './pages/problematics/Problematics'; 
 
 
 const router = createBrowserRouter([
   { path: "/", element: <Welcome /> },
+
+
   // { path: "/login", element: <Login /> },
+  { path:"/problematics", element:<Problematicas />},
   { path: "/home", element: <Home /> },
   { path: "*", element: <Page404 /> },
 ]);
