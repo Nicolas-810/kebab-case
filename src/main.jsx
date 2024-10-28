d0e974f27c7ffdc6a315dd0785d702c21aa6c9f4
Nicolas Taborda Gomez <nicolas.taborda@univalle.edu.co>
2024-10-27 20:09:13 -0500

    Implementacion de muchos cambios

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Welcome from "./Pages/Welcome/Welcome";
-// import Login from "./pages/Login/Login";
 import Page404 from "./pages/page404/page404";
 import Home from "./pages/home/Home";
+import Welcome from "./pages/welcome/Welcome";
 import "./index.css"
-
 
 const router = createBrowserRouter([
   { path: "/", element: <Welcome /> },
-  // { path: "/login", element: <Login /> },
   { path: "/home", element: <Home /> },
   { path: "*", element: <Page404 /> },
 ]);
--- src/pages/page404/page404.jsx
+++ src/pages/page404/page404.jsx
@@ -1,14 +1,31 @@
-import Links from "../../components/Links";
+import { useNavigate } from "react-router-dom";
 import './page404.css';
+import groupLogo from '../../assets/Icon.png'; // Asegúrate de importar el logo
+import centerImage from '../../assets/Error404.png'; // Asegúrate de tener esta imagen
 
 const Page404 = () => {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/Home");
+  };
+
   return (
-    <>
-      <Links />
-      <div className="container">
-        <p>Lo sentimos, la página que buscas no existe.</p>
+    <div className="container-404">
+      <header className="navbar-container">
+        <div className="logo-section">
+          <img src={groupLogo} alt="Logo del proyecto" className="logo" />
+          <h3 className="project-title">HYDRONET</h3>
+        </div>
+        <button className="home-button" onClick={handleGoHome}>
+          Volver a página inicio
+        </button>
+      </header>
+      
+      <div className="content">
+        <img src={centerImage} alt="Imagen de error 404" className="center-image" />
       </div>
-    </>
+    </div>
   );
 };
 
