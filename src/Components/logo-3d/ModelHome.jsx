c4e13032696145c743aaa227824dfd6a203f1482
Nicolas Taborda Gomez <nicolas.taborda@univalle.edu.co>
2024-11-10 15:31:07 -0500

    Implematacion de modelo en scarcity y sky en home

---
+++ src/Components/logo-3d/ModelHome.jsx
@@ -0,0 +1,24 @@
+import { useGLTF } from "@react-three/drei";
+
+const Home3D = (props) => {
+    const { nodes, materials } = useGLTF('../models-3D/AngelFish.glb')
+
+    return (
+        <group {...props} dispose={null}>
+          <group name="Scene">
+            <mesh
+              name="AngelFish_Royal_Instance_Wander"
+              geometry={nodes.AngelFish_Royal_Instance_Wander.geometry}
+              material={materials.AngelFish_Royal_Instanced_Indirect}
+              position={[0.556, -0.604, -0.431]}
+              rotation={[Math.PI / 2, 0, 0]}
+              scale={37.253}
+            />
+          </group>
+        </group>
+    )
+};
+    
+useGLTF.preload('../models-3D/AngelFish.glb')
+
+export default Home3D;
