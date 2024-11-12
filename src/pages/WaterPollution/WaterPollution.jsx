0597e33bb02428ec800704a15165760498c92130
Sebastian Albaran Maya <sebastian.albaran@correounivalle.edu.co>
2024-11-11 21:05:34 -0500

    solución merge

--- src/pages/WaterPollution/WaterPollution.jsx
+++ src/pages/WaterPollution/WaterPollution.jsx
@@ -15,7 +15,7 @@ const WaterPollution = () => {
 
   const goNext = () => {
     navigate("/WaterScarcity");
-  };
+  };  
 
   return (
     <div className="home-page-pullution">
@@ -133,7 +133,7 @@ const WaterPollution = () => {
       </div>
       <div className="house3D-container">
         <Canvas camera={{ position: [10, 5, 15], fov: 85 }}>
-          <TitleWaterPollution />
+          <Html3DWaterPollution />
           <ambientLight intensity={0.5} />
           <directionalLight position={[10, 10, 5]} intensity={1} />
           <House3D position={[-10, 0, -17]} />
